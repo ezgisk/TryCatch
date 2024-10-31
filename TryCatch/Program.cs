b6265625fd13d1aec4cad44e3a717fa9141c2783
Gorkem <Gorkem@DESKTOP-2DV200H>
2024-10-30 19:15:28 -0700

    Add project files.

---
+++ TryCatch/Program.cs
@@ -0,0 +1,16 @@
+﻿try
+{
+    Console.WriteLine("Lutfen bir sayi giriniz:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Girdiginiz sayinin karesi:{ Math.Pow(number,2)}");
+    Console.WriteLine($"Girdiginiz sayinin karekoku:{Math.Sqrt(number)}");
+}
+catch
+{
+    Console.WriteLine("Hata: Sayı formatı geçersiz. Lütfen doğru formatta bir sayı giriniz.");//Hata yapilirsa gosterilir
+}
+finally
+{
+    Console.WriteLine("İşlem tamamlandı.");//Her zaman calisir
+}
+Console.ReadKey();
